f675b750cef6672cfebd75d09cda32036b10ea6c
Thulisile Ngomane <thuleen@outlook.com>
2023-05-10 00:32:18 +0200

    fix

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 
-""" retrieve the top ten hot topic on reddit"""
+""" retrieves the top ten hot topic on reddit"""
 
 import requests
 
 
 def top_ten(subreddit):
-    """ get the top ten hot topics"""
+    """ gets the top ten hot topics"""
 
     url = "reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {"User-Agent": 'My agent'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
-        posts = response.json()
+    posts = requests.get(url, headers=headers, allow_redirects=False)
 
-        for post in posts['data']['children']:
+    if post.status_code == 200:
+        for post in posts.json()['data']['children']:
             print(post['data']['title'])
     else:
         return (None)
