e18aaf91a02d09dd3584bcb368ad2afdaad96dc6
Thulisile Ngomane <thuleen@outlook.com>
2023-05-09 19:39:28 +0200

    fixed indentation error

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,16 @@
-#!/bin/python3
+#!/usr/bin/python3
 
 """script to retrieve the number of subscribers on subreddit"""
 
 import requests
 
+
 def number_of_subscribers(subreddit):
-     """returns the number of subscribers"""
+    """returns the number of subscribers"""
 
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": 'My Agent'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    data = response.json()
 
     if response.status_code == 200:
         data = response.json()
