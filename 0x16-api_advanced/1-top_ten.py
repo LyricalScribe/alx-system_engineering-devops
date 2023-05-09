1509312511867cdf0e21f29a9494b44f2e8eda41
Thulisile Ngomane <thuleen@outlook.com>
2023-05-09 21:33:36 +0200

    top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,19 +1,21 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-    """retrieve top ten hot topic on reddit """
+""" retrieve the top ten hot topic on reddit """
+
 import requests
 
+
 def top_ten(subreddit):
-     """get top ten hot topic"""
+    """ get the top ten hot topics"""
 
     url = "reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers ={"User-Agent": 'My agent'}
-    response = request.get(url, headers=headers, allow_redirects=False)
+    headers = {"User-Agent": 'My agent'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        post = response.json()
+        posts = response.json()
 
-        for post in posts('data')['children']:
+        for post in posts['data']['children']:
             print(post['data']['title'])
     else:
-        return 0
+        return None
