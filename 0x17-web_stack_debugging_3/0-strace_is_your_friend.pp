9290e4e63e8ebd6cc9c967e7df1025b21d216c06
Thulisile Ngomane <thuleen@outlook.com>
2023-05-10 23:41:49 +0200

    script edit

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,10 +1,10 @@
- Puppet script to edit typo on php file
+# Puppet script to edit typo on php file
 
 $file_path = '/var/www/html/wp-settings.php'
 
 # edit "phpp" to "php"
 
-exec { 'replace_line':
+exec { 'edit_typo':
   command => "sed -i 's/phpp/php/g' ${file_path}",
   path    => ['/bin','/usr/bin']
 }
